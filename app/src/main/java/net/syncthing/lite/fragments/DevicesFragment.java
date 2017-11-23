112bdf6376b773daf08e662f88731a660f8dba9c
Felix Ableitner <me@nutomic.com>
2017-11-23 23:30:31 +0900

    Allow entering device ID as text

--- app/src/main/java/net/syncthing/lite/fragments/DevicesFragment.java
+++ app/src/main/java/net/syncthing/lite/fragments/DevicesFragment.java
@@ -1,6 +1,7 @@
 package net.syncthing.lite.fragments;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
@@ -12,6 +13,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -23,9 +26,15 @@ import net.syncthing.lite.adapters.DevicesAdapter;
 import net.syncthing.lite.databinding.FragmentDevicesBinding;
 import net.syncthing.lite.utils.UpdateIndexTask;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.security.InvalidParameterException;
+
 import it.anyplace.sync.core.beans.DeviceInfo;
 import it.anyplace.sync.core.beans.DeviceStats;
 import it.anyplace.sync.core.security.KeystoreHandler;
+import uk.co.markormesher.android_fab.SpeedDialMenuAdapter;
+import uk.co.markormesher.android_fab.SpeedDialMenuItem;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
@@ -43,7 +52,7 @@ public class DevicesFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.fragment_devices, container, false);
         mBinding.list.setEmptyView(mBinding.empty);
-        mBinding.scanQrCode.setOnClickListener(view -> new FragmentIntentIntegrator(this).initiateScan());
+        mBinding.fab.setSpeedDialMenuAdapter(new FabMenuAdapter());
         return mBinding.getRoot();
     }
 
@@ -92,7 +101,13 @@ public class DevicesFragment extends Fragment {
     }
 
     private void importDeviceId(String deviceId) {
-        KeystoreHandler.validateDeviceId(deviceId);
+        try {
+            KeystoreHandler.validateDeviceId(deviceId);
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(getContext(), R.string.invalid_device_id, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         boolean modified = mActivity.getConfiguration().edit().addPeers(new DeviceInfo(deviceId, null));
         if (modified) {
             mActivity.getConfiguration().edit().persistLater();
@@ -102,6 +117,47 @@ public class DevicesFragment extends Fragment {
                     .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         } else {
             Toast.makeText(getContext(), "device already present: " + deviceId, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class FabMenuAdapter extends SpeedDialMenuAdapter {
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @NotNull
+        @Override
+        public SpeedDialMenuItem getMenuItem(Context context, int i) {
+            switch (i) {
+                case 0: return new SpeedDialMenuItem(getContext(), R.drawable.ic_qr_code_white_24dp, R.string.scan_qr_code);
+                case 1: return new SpeedDialMenuItem(getContext(), R.drawable.ic_edit_white_24dp, R.string.enter_device_id);
+            }
+            throw new InvalidParameterException();
+        }
+
+        @Override
+        public boolean onMenuItemClick(int position) {
+            switch (position) {
+                case 0:
+                    new FragmentIntentIntegrator(DevicesFragment.this).initiateScan();
+                    break;
+                case 1:
+                    EditText editText = new EditText(getContext());
+                    AlertDialog dialog = new AlertDialog.Builder(getContext())
+                            .setTitle(R.string.device_id_dialog_title)
+                            .setView(editText)
+                            .setPositiveButton(android.R.string.ok, (dialogInterface, i) ->
+                                    importDeviceId(editText.getText().toString()))
+                            .setNegativeButton(android.R.string.cancel, null)
+                            .create();
+                    dialog.setOnShowListener(dialogInterface -> {
+                        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+                    });
+                    dialog.show();
+            }
+            return true;
         }
     }
 }
