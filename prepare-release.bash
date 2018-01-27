ef2d7fe9d73211b7a128358abb3c4f1f6bac6eef
Felix Ableitner <me@nutomic.com>
2018-01-27 17:29:40 +0900

    Updated release script

--- prepare-release.bash
+++ prepare-release.bash
@@ -3,7 +3,7 @@
 set -e
 
 NEW_VERSION_NAME=$1
-OLD_VERSION_NAME=$(grep "versionName" "app/build.gradle" | awk '{print $2}')
+OLD_VERSION_NAME=$(grep "versionName" "app/build.gradle" | awk '{print $2}' | tr -d "\"")
 if [[ -z ${NEW_VERSION_NAME} ]]
 then
     echo "New version name is empty. Please set a new version. Current version: $OLD_VERSION_NAME"
@@ -27,26 +27,27 @@ fi
 
 echo "
 
-Running Lint
------------------------------
-"
-./gradlew clean lintVitalRelease
-
-
-echo "
-
 Updating Version
 -----------------------------
 "
 OLD_VERSION_CODE=$(grep "versionCode" "app/build.gradle" -m 1 | awk '{print $2}')
 NEW_VERSION_CODE=$(($OLD_VERSION_CODE + 1))
 sed -i "s/versionCode $OLD_VERSION_CODE/versionCode $NEW_VERSION_CODE/" "app/build.gradle"
+sed -i "s/versionName \"$OLD_VERSION_NAME\"/versionName \"$NEW_VERSION_NAME\"/" "app/build.gradle"
 
-OLD_VERSION_NAME=$(grep "versionName" "app/build.gradle" | awk '{print $2}')
-sed -i "s/$OLD_VERSION_NAME/\"$NEW_VERSION_NAME\"/" "app/build.gradle"
+LIBRARY_NAME="com.github.Nutomic:syncthing-java"
+sed -i "s/$LIBRARY_NAME:$OLD_VERSION_NAME/$LIBRARY_NAME:$NEW_VERSION_NAME/" "app/build.gradle"
+
 git add "app/build.gradle"
 git commit -m "Version $NEW_VERSION_NAME"
 git tag ${NEW_VERSION_NAME}
+
+echo "
+
+Running Lint
+-----------------------------
+"
+./gradlew clean lintVitalRelease
 
 echo "
 Update ready.
